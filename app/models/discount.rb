06bd627269dfdf5b6cf46eb8ae2e96a03386e257
leugiim <miguelrl.92@gmail.com>
2024-02-07 01:00:04 +0100

    fix discounts

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -3,7 +3,7 @@ class Discount < ApplicationRecord
 
   validates :strategy, inclusion: { in: %w[NxM Flat Percent] }
   validates :quantity, numericality: { greater_than: 0 }, presence: true
-  validates :discount, numericality: { greater_than: 0 }, presence: true
+  validates :discount, numericality: { greater_than_or_equal_to: 0 }, presence: true
 
   scope :by_product, ->(product) { where(product:) }
 end
